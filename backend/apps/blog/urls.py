5c075f99fc0ce8babe511f4ec598f709db46d379
63phc <pavel.burns@gmail.com>
2018-10-31 01:09:33 +0500

    clear old files

--- backend/apps/blog/urls.py
+++ backend/apps/blog/urls.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
-    # path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
     # path(r'^posts/$', PostList.as_view(), name='post_list'),
     
     # List posts
--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,34 +1,19 @@
-"""lks_dev URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
-from apps.blog import views
+# from rest_framework import routers
+# from apps.blog import urls
 
-router = routers.DefaultRouter()
-router.register(r'posts', views.ArticleList)
+# router = routers.DefaultRouter()
+# router.register(r'posts', views.ArticleList)
 
 urlpatterns = [
 
     path('admin/', admin.site.urls),
-    path(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
-    # path('/blog', include('blog.urls')),
+    path('', include('apps.blog.urls')),
     # path('/shop', include('shop.urls')),
 
 ]
-urlpatterns += router.urls
+# urlpatterns += router.urls
 # http://www.django-rest-framework.org/api-guide/routers/#usage
