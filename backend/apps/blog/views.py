b64df5387eade314bfaa544032914a37b5c262f4
63phc <pavel.burns@gmail.com>
2018-11-03 00:08:04 +0500

    small fix

--- backend/apps/blog/views.py
+++ backend/apps/blog/views.py
@@ -4,21 +4,21 @@ from rest_framework.viewsets import ModelViewSet
 from .models import Article
 from .serializers import ArticleSerializer
 
-# ModelViewSet ListAPIView
+
 class ArticleList(ModelViewSet):
     queryset = Article.objects.prefetch_related('tags').all()
     serializer_class = ArticleSerializer
 
 
-class ListView(TemplateView):
-    template_name = 'index.html'
+class BlogListView(TemplateView):
+    template_name = 'blog/list.html'
 
     def get_context_data(self, **kwargs):
-        context = super(ListView, self).get_context_data(**kwargs)
+        context = super(BlogListView, self).get_context_data(**kwargs)
         context['articles'] = Article.objects.all().order_by('?')[:4]
         return context
 
 
-class DetailView(DetailView):
+class BlogDetailView(DetailView):
     model = Article
     template_name = 'detail.html'
