e08ee024029c6d4891cf288252c960a0d58217ca
Ravis <aao.t@yandex.ru>
2018-11-03 22:14:52 +0300

    new templates were used

--- backend/apps/shop/views.py
+++ backend/apps/shop/views.py
@@ -6,7 +6,7 @@ from .models.product import Product
 
 class ProductListView(ListView):
     model = Product
-    template_name = 'product_list.html'
+    template_name = 'shop/product_list.html'
 
     def get_queryset(self):
         return Product.objects.all()
@@ -14,14 +14,24 @@ class ProductListView(ListView):
 
 class ProductDetailView(DetailView):
     model = Product
-    template_name = 'product_detail.html'
+    template_name = 'shop/product_detail.html'
 
 
 class CategoryProductsListView(ListView):
     model = Category
-    template_name = 'category_products.html'
+    template_name = 'shop/category_product_list.html'
 
     def get_queryset(self, *args, **kwargs):
         return Product.objects.filter(
             category__slug=self.kwargs['slug'], active=True
         )
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(
+            CategoryProductsListView, self
+        ).get_context_data(**kwargs)
+        context['category'] = Category.objects.get(
+            slug=self.kwargs['slug']
+        ).title.lower()
+        return context
+
