6a3735a0636706c87272a683bd394c910656aa02
postman17 <frompostal@gmail.com>
2018-11-08 14:48:28 +0500

    add tests

--- backend/apps/subscribe/tests.py
+++ backend/apps/subscribe/tests.py
@@ -1,7 +1,6 @@
 from django.test import TestCase, Client
 from django.conf import settings
 from apps.subscribe.models import Subscribe
-from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
 
 
@@ -9,19 +8,25 @@ class TestSubscribe(TestCase):
 
     def setUp(self):
         self.client = Client()
+        Subscribe.objects.create(email='fortest@test.com')
 
 
     def test_app_in_settings(self):
         self.assertTrue('apps.subscribe' in settings.INSTALLED_APPS)
 
-    def test_subscribe_view(self):
+    def test_subscribe_with_right_email(self):
         url = reverse_lazy('subscribe:ajax_subscribe')
         response = self.client.post(url, {'email': 'test@test.com'})
+        data = response.json()
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['response'], 'success')
 
-    # def test_delete_record(self):
-    #     data = get_object_or_404(Subscribe, email='test@test.com')
-    #     print(data)
-    #     data.delete()
+    def test_subscribe_with_wrong_email(self):
+        url = reverse_lazy('subscribe:ajax_subscribe')
+        response = self.client.post(url, {'email': 'fortest@test.com'})
+        data = response.json()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['response'], 'Вы уже подписаны.')
+
 
 
