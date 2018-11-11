8f27e6ebad7c7a64b78344f42bfb43f341c16f91
postman17 <frompostal@gmail.com>
2018-11-11 12:07:08 +0500

    corrections

--- backend/apps/tags/views.py
+++ backend/apps/tags/views.py
@@ -2,14 +2,15 @@ from django.views.generic import ListView
 from django.shortcuts import render, get_object_or_404
 from apps.blog.models import Article
 from .models import Tag
+from django.conf import settings
 
 
 class TagsList(ListView):
-    def get(self, request, slug):
-        context = {}
-        tags = get_object_or_404(Tag, slug=slug)
-        context['object_list'] = Article.objects.filter(tags=tags)
-        return render(request, 'blog/list.html', context)
+    model = Tag
+    template_name = 'blog/list.html'
+    paginate_by = settings.PAGINATION_BY
 
-
-
+    def get_queryset(self, **kwargs):
+        tags = get_object_or_404(Tag, slug=self.kwargs['slug'])
+        queryset = Article.objects.filter(tags=tags)
+        return queryset
