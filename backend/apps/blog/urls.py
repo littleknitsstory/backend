2136b7b7b8cd3543cca9eaf75bf2d72691407def
postman17 <frompostal@gmail.com>
2018-11-13 14:45:48 +0500

    add change

--- backend/apps/blog/urls.py
+++ backend/apps/blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import BlogListView, BlogDetailView
 
+app_name='blog'
+
 urlpatterns = [
     path('', BlogListView.as_view(), name='blog-list'),
     path('blog/<slug:slug>/', BlogDetailView.as_view(), name='blog-detail'),
