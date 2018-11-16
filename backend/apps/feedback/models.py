2b44aa9d1ddb36f8c3ecddb2b4c74299a801c8e1
Ravis <aao.t@yandex.ru>
2018-11-16 16:38:43 +0300

    sending new feedback message by bot to Telegram clients

--- backend/apps/feedback/models.py
+++ backend/apps/feedback/models.py
@@ -1,12 +1,23 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+from backend.telegram_bot.bot import send_message
+
 
 class Feedback(models.Model):
     """ Feedback model """
     name = models.CharField(_('Name'), max_length=120)
     email = models.EmailField(_('Email'))
     feedback = models.TextField(_('Feedback'))
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        super(Feedback, self).save(
+            force_insert, force_update, using, update_fields
+        )
+        send_message(
+            "Новое обращение к администрации сайта:\n" + self.feedback
+        )
 
     class Meta:
         verbose_name = _('Feedback')
