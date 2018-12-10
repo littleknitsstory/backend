791fe62077a173fcd6c4931e44a6c2145b96c7f2
63phc <pavel.burns@gmail.com>
2018-12-10 21:40:17 +0500

    migrations | wabpack conf | fixtures shop

--- backend/core/settings/components/webpack.py
+++ backend/core/settings/components/webpack.py
@@ -1,9 +1,26 @@
 import os
 from django.conf import settings
 
+# WEBPACK_LOADER = {
+#     'DEFAULT': {
+#         'BUNDLE_DIR_NAME': 'builds/',
+#         'STATS_FILE': os.path.join(settings.ROOT_DIR, './static/webpack.scss.json'),
+#     }
+# }
+
+
 WEBPACK_LOADER = {
     'DEFAULT': {
         'BUNDLE_DIR_NAME': 'builds/',
-        'STATS_FILE': os.path.join(settings.ROOT_DIR, './static/webpack.scss.json'),
+        'CACHE': not settings.DEBUG,
+        'POLL_INTERVAL': 0.1,
+        'IGNORE': ['.+\.hot-update.js', '.+\.map']
     }
 }
+
+WEBPACK_LOADER.update({
+    'DEFAULT': {
+        'BUNDLE_DIR_NAME': 'builds/',  # must end with slash
+        'STATS_FILE': os.path.join(settings.ROOT_DIR, './static/webpack.scss.json'),
+    }
+})
