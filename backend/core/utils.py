5a8834c7708681292bfd8d2c511312a7007fd1c4
Ravis <aao.t@yandex.ru>
2018-12-13 22:13:42 +0300

    watermark function

---
+++ backend/core/utils.py
@@ -0,0 +1,31 @@
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+
+def watermark_text(input_image_path, output_image_path, text, pos):
+    """
+    It print the text right on the input image
+    :param input_image_path:
+    :param output_image_path:
+    :param text:
+    :param pos: it should contain of a tuple with X and Y positions.
+    Example: (0,0) is a top left corner of the image
+    :return:
+    """
+    photo = Image.open(input_image_path)
+
+    # make the image editable
+    drawing = ImageDraw.Draw(photo)
+
+    black = (3, 8, 12)
+    font = ImageFont.truetype("Pillow/Tests/fonts/FreeMono.ttf", 40)
+    drawing.text(
+        pos, text,
+        fill=black,
+        font=font
+    )
+    photo.show()
+    photo.save(output_image_path)
+
+
