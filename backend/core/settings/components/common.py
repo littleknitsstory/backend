8c9c15998775784d97622a9f2161fc53b86592ad
Ravis <aao.t@yandex.ru>
2018-12-19 11:34:41 +0300

    comments

--- backend/core/settings/components/common.py
+++ backend/core/settings/components/common.py
@@ -156,7 +156,10 @@ CKEDITOR_CONFIGS = {
     },
 }
 
-# use loggin.error('message') or loggin.info('message') to write into the log
+# use loggin.error('message') or loggin.info('message') just to write into
+# the log, but more efficient to use separated logger in every module:
+# logger = getLogger(__name__)
+# logger.info(string), .warning(string) and so on
 logging.basicConfig(
     filename=os.path.join(ROOT_DIR, 'lks_backend.log'), level=logging.DEBUG
 )
