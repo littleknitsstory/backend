64d74309002a4d012362c793f13dc2dc7014af14
Ravis <aao.t@yandex.ru>
2018-12-24 11:12:41 +0300

    comments are showed but are not created

--- backend/apps/blog/urls.py
+++ backend/apps/blog/urls.py
@@ -5,7 +5,7 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='blog-list'),
-    path('blog/comments/', include('fluent_comments.urls')),
+
     path('blog/<slug:slug>/', BlogDetailView.as_view(), name='blog-detail'),
     path('blog/author/<author>/', BlogListView.as_view(), name='author-list'),
 ]
