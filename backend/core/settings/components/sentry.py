46f347dc50303f09db7b7d96e0bb6e02d1ad180d
63phc <pavel.burns@gmail.com>
2019-06-05 11:59:03 +0500

    add sentry

---
+++ backend/core/settings/components/sentry.py
@@ -0,0 +1,7 @@
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="https://e396c654fac2472f8fd2117e49ca1164@sentry.io/1474916",
+    integrations=[DjangoIntegration()]
+)
