3d724b769baba97de5054a9c1a8cf31e74a5e163
63phc <pavel.burns@gmail.com>
2020-02-20 12:21:18 +0100

    [add] CORS and fix fixture

--- src/settings/components/common.py
+++ src/settings/components/common.py
@@ -14,8 +14,8 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 CSRF_COOKIE_NAME = "XCSRF-Token"
-# CORS_ORIGIN_ALLOW_ALL = True
-# CORS_ORIGIN_WHITELIST = ['*']
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = ['*']
 
 ROOT_URLCONF = 'src.core.urls'
 WSGI_APPLICATION = 'src.core.wsgi.application'
