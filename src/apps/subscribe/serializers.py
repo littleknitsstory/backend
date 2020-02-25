f068f868957f4d72774b74ed39f647f963a2ed9b
63phc <pavel.burns@gmail.com>
2020-02-25 13:15:30 +0100

    [add] valid email

--- src/apps/subscribe/serializers.py
+++ src/apps/subscribe/serializers.py
@@ -1,12 +1,15 @@
+from django.core.validators import EmailValidator
 from rest_framework import serializers
 
 from src.apps.subscribe.models import Subscribe
 
 
 class SubscribeSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(validators=[EmailValidator()])
+    
     class Meta:
         model = Subscribe
         fields = ('email', )
         
-    def validate_email(self):
+    def validate_email(self, obj):
         pass
