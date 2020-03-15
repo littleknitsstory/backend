2f2f47351b279ab5b9c8b6cee01109924a076b23
63phc <pavel.burns@gmail.com>
2020-03-15 20:38:22 +0100

    [del]     "debug_toolbar.middleware.DebugToolbarMiddleware",

--- src/settings/components/middleware.py
+++ src/settings/components/middleware.py
@@ -8,5 +8,4 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
