54101f2d45e87d07644964cd98882f64d8e0696b
63phc <pavel.burns@gmail.com>
2020-03-15 20:19:28 +0100

    [add] cache.py

--- src/settings/components/cache.py
+++ src/settings/components/cache.py
@@ -1,25 +1,23 @@
-# from decouple import config
-#
-# REDIS_PASSWORD = config('REDIS_PASSWORD')
-#
-# CACHES = {
-#     'default': {
-#         'BACKEND': 'django_redis.cache.RedisCache',
-#         'LOCATION': [
-#             'redis://{}:{}/1'.format(config('REDIS_HOST'), config('REDIS_PORT'))
-#         ],
-#         'OPTIONS': {
-#             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-#             'CONNECTION_POOL_CLASS': 'redis.BlockingConnectionPool',
-#             'CONNECTION_POOL_CLASS_KWARGS': {
-#                 'max_connections': 50,
-#                 'timeout': 20,
-#             },
-#             'MAX_CONNECTIONS': 1000,
-#             'PICKLE_VERSION': -1,
-#         },
-#     },
-# }
-#
-# if REDIS_PASSWORD:
-#     CACHES['default']['OPTIONS']['PASSWORD'] = REDIS_PASSWORD
+from decouple import config
+
+REDIS_HOST = config('REDIS_HOST')
+REDIS_PORT = config('REDIS_PORT')
+REDIS_PASSWORD = config('REDIS_PASSWORD')
+
+CACHES = {
+	"default": {
+		"BACKEND": "django_redis.cache.RedisCache",
+		"LOCATION": f"redis://{REDIS_HOST}:{REDIS_PORT}/1",
+		"OPTIONS": {
+			"CLIENT_CLASS": "django_redis.client.DefaultClient",
+			'CONNECTION_POOL_CLASS_KWARGS': {
+				'max_connections': 50,
+				'timeout': 20,
+			},
+			'MAX_CONNECTIONS': 1000,
+			'PICKLE_VERSION': -1,
+		}
+	}
+}
+if REDIS_PASSWORD:
+	CACHES['default']['OPTIONS']['PASSWORD'] = REDIS_PASSWORD
