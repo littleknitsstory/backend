d97815240e9deb4ffb122a8489f5f1e540e896c9
Oleg Khvostenko <xboctehko@yandex.ru>
2022-12-02 23:02:46 +0100

    Fixed: cooments/choises.py

--- src/apps/comments/choices.py
+++ src/apps/comments/choices.py
@@ -1,3 +1,5 @@
+from django.utils.translation import gettext_lazy as _
+
 
 class CommentAssociationChoices:
     ARTICLE = "ARTICLE"
@@ -6,8 +8,8 @@ class CommentAssociationChoices:
     PRODUCT = "PRODUCT"
 
     MODEL_CHOICES = (
-        (ARTICLE, "Article"),
-        (COMMENT, "Comment"),
-        (COURSE, "Course"),
-        (PRODUCT, "Product"),
+        (ARTICLE, _("Article")),
+        (COMMENT, _("Comment")),
+        (COURSE, _("Course")),
+        (PRODUCT, _("Product")),
     )
