5bb0dc83c9822d326b485933540ffcf6be32435f
vbamnup <vbamnup@gmail.com>
2023-05-20 15:09:04 +0300

    Add killer feature. Return response from open ai, convert response to pdf

--- src/apps/patterns/urls.py
+++ src/apps/patterns/urls.py
@@ -1,14 +1,19 @@
 from django.urls import path, include
 from rest_framework import routers
 
+from src.apps.patterns.viewsets import ChatCompletionViewSet
 from src.apps.patterns.viewsets import PatternViewset
-
 
 app_name = "patterns"
 
 
 router = routers.DefaultRouter()
 router.register(r"patterns", PatternViewset)
+router.register(
+    r"patterns/generate_pdf/(?P<uuid>[^/.]+)",
+    ChatCompletionViewSet,
+    basename="generate_pdf",
+)
 
 
 urlpatterns = [
--- src/apps/patterns/viewsets.py
+++ src/apps/patterns/viewsets.py
@@ -1,11 +1,23 @@
-from rest_framework.permissions import AllowAny
+from io import BytesIO
+
+from django.http import HttpResponse
+from jinja2 import Template
+from django.template import Context, Template
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
-from .serializers import PatternSerializer
-from .models import Pattern
+from rest_framework.viewsets import ViewSet
+from xhtml2pdf import pisa
+
+from src.apps.patterns.models import Pattern
+from src.apps.patterns.serializers import DataSerializer, PatternSerializer
+from src.apps.patterns.services import fetch_data_from_api
 
 
 class PatternViewset(ModelViewSet):
-    permission_classes = (AllowAny,)
+    permission_classes = (IsAuthenticated,)
     queryset = Pattern.objects.all()
     http_method_names = ["post", "get"]
     lookup_field = "uuid"
@@ -15,3 +27,60 @@ class PatternViewset(ModelViewSet):
 
     def get_serializer_class(self):
         return self.serializer_classes.get(self.action, PatternSerializer)
+
+    def create(self, request, *args, **kwargs):
+        serializer = PatternSerializer(data=request.data)
+        if serializer.is_valid():
+            prompt = serializer.validated_data["prompt"]
+            completion = fetch_data_from_api(prompt)
+            pattern = serializer.save(response=completion)
+            return Response(
+                {
+                    "uuid": pattern.uuid,
+                    "author": request.user.username,
+                    "prompt": pattern.prompt,
+                    "response": pattern.response,
+                },
+                status=status.HTTP_201_CREATED,
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ChatCompletionViewSet(ViewSet):
+    serializer = DataSerializer
+    renderer_classes = (TemplateHTMLRenderer,)
+
+    def list(self, request, uuid=None):
+        pattern = Pattern.objects.get(uuid=uuid)
+        template_str = """<!DOCTYPE html>
+                <html>
+                    <head>
+                        <style>
+                            body {
+                                font-family: DejaVu Sans, sans-serif;
+                            }
+                            p {
+                                font-family: 'Times New Roman', Times, serif;
+                            }
+                        </style>
+                    </head>
+                    <body>
+                       <p>{{ content }}</p>
+                    </body>
+                </html>"""
+        template = Template(template_str)
+        serializer = self.serializer(data=pattern.response)
+        serializer.is_valid(raise_exception=True)
+        data = serializer.validated_data
+        content = data["choices"][0]["message"]["content"]
+        context = Context({"content": content})
+        rendered_template = template.render(context)
+        pdf_file = BytesIO()
+        pisa.CreatePDF(
+            BytesIO(rendered_template.encode("UTF-8")), pdf_file, encoding="UTF-8"
+        )
+
+        response = HttpResponse(pdf_file.getvalue(), content_type="application/pdf")
+        response["Content-Disposition"] = 'attachment; filename="schema.pdf"'
+
+        return response
